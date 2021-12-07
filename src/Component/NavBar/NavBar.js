1ace2b4badddb74f0a464ec23623ef9ca4360394
mahfuz60 <mahfuzuralam1996@gmail.com>
2021-12-07 14:12:45 +0600

    navbar code

---
+++ src/Component/NavBar/NavBar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import istLogo from "../../images/istLogo.png";
+import "./NavBar.css";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Button,
+  FormControl,
+  Form,
+} from "react-bootstrap";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar className="navBar" bg='dark' variant="dark" expand="md">
+        <Container>
+          <img
+            className="logoImg"
+            style={{ width: "120px", height: "120px", paddingRight: "10px" }}
+            src={istLogo}
+            alt=""
+          />
+          <Navbar.Brand style={{ fontSize: "25px" }}>
+            IST Online Library
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link to="/" className="navLink">
+                Home
+              </Nav.Link>
+              <Nav.Link to="/books" className="navLink">
+                Books
+              </Nav.Link>
+              <Nav.Link to="/contact" className="navLink">
+                Contact
+              </Nav.Link>
+
+              <Nav.Link to="/login" className="navLink">
+                Login
+              </Nav.Link>
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="md-2"
+                  aria-label="Search"
+                />
+                <Button style={{marginLeft:"5px",fontSize:"20px"}} variant="outline-warning ">Search</Button>
+              </Form>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
